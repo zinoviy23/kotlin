ce4111a7ab341dc8e4fc2d02ed75a95cb301be24
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2019-09-23 10:49:48 +0300

    Tests: rewrite some AbstractScriptConfigurationTest to the new script infrastructure

--- idea/testData/script/definition/defaultTemplate/template.kt
+++ idea/testData/script/definition/defaultTemplate/template.kt
@@ -1,23 +1,59 @@
 package org.jetbrains.kotlin.idea.script
 
 import java.io.File
-import kotlin.script.dependencies.*
-import kotlin.script.experimental.dependencies.*
-import kotlin.script.experimental.location.*
-import kotlin.script.templates.ScriptTemplateDefinition
+import kotlin.script.experimental.annotations.KotlinScript
+import kotlin.script.experimental.api.*
+import kotlin.script.experimental.host.ScriptingHostConfiguration
+import kotlin.script.experimental.host.ScriptingHostConfigurationKeys
+import kotlin.script.experimental.jvm.JvmDependency
+import kotlin.script.experimental.jvm.dependenciesFromClassContext
+import kotlin.script.experimental.jvm.jdkHome
+import kotlin.script.experimental.jvm.jvm
+import kotlin.script.experimental.util.PropertiesCollection
 
-class FromTextDependenciesResolver : DependenciesResolver {
-    @Suppress("UNCHECKED_CAST")
-    override fun resolve(scriptContents: ScriptContents, environment: Environment): DependenciesResolver.ResolveResult {
-        return ScriptDependencies(
-            javaHome = environment["javaHome"] as? File,
-            classpath = (environment["classpath"] as? List<File>).orEmpty(),
-            imports = (environment["imports"] as? List<String>).orEmpty(),
-            sources = (environment["sources"] as? List<File>).orEmpty()
-        ).asSuccess()
+@KotlinScript(
+    displayName = "Definition for tests",
+    fileExtension = "kts",
+    compilationConfiguration = TemplateDefinition::class
+)
+open class Template(val args: Array<String>)
+
+val ScriptingHostConfigurationKeys.getEnvironment by PropertiesCollection.key<() -> Map<String, Any?>?>()
+
+object TemplateDefinition : ScriptCompilationConfiguration(
+    {
+        jvm {
+            dependenciesFromClassContext(TemplateDefinition::class)
+        }
+        ide {
+            acceptedLocations(ScriptAcceptedLocation.Everywhere)
+        }
+        refineConfiguration {
+            beforeCompiling { context ->
+                val environment = context.compilationConfiguration[ScriptCompilationConfiguration.hostConfiguration]?.let {
+                    it[ScriptingHostConfiguration.getEnvironment]?.invoke()
+                }.orEmpty()
+
+                ScriptCompilationConfiguration(context.compilationConfiguration) {
+                    val javaHome = (environment["javaHome"] as? File)
+                    if (javaHome != null) {
+                        jvm.jdkHome.put(javaHome)
+                    }
+                    val imports = (environment["imports"] as? List<String>).orEmpty()
+                    for (entry in imports) {
+                        defaultImports.append(entry)
+                    }
+                    val classPath = (environment["classpath"] as? List<File>).orEmpty()
+                    for (entry in classPath) {
+                        dependencies.append(JvmDependency(entry))
+                    }
+                    val sources = (environment["sources"] as? List<File>).orEmpty()
+                    for (entry in sources) {
+                        ide.dependenciesSources.append(JvmDependency(entry))
+                    }
+                }.asSuccess()
+            }
+        }
     }
-}
+)
 
-@ScriptExpectedLocations([ScriptExpectedLocation.Everywhere])
-@ScriptTemplateDefinition(FromTextDependenciesResolver::class, scriptFilePattern = "script.kts")
-open class Template
