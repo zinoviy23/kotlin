868f2deb97c024e42c86ad0f4c7f19985e1246a2
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-10-10 14:39:27 +0300

    Add CLI test for K/javac project with K/J interdependency
    Yet this test fails if JavacWrapper does not add Kotlin output dir to classpath

--- compiler/javac-wrapper/src/org/jetbrains/kotlin/javac/JavacWrapper.kt
+++ compiler/javac-wrapper/src/org/jetbrains/kotlin/javac/JavacWrapper.kt
@@ -170,10 +170,9 @@ class JavacWrapper(
         setClassPathForCompilation(outDir)
         makeOutputDirectoryClassesVisible()
 
-        context.get(Log.outKey)?.println(
-            "Compiling $javaFilesNumber Java source files" +
-                    " to [${fileManager.getLocation(CLASS_OUTPUT)?.firstOrNull()?.path}]"
-        )
+        val outputPath =
+            fileManager.getLocation(CLASS_OUTPUT)?.firstOrNull()?.path?.takeIf { "compilerProject_test" !in it } ?: "test directory"
+        context.get(Log.outKey)?.println("Compiling $javaFilesNumber Java source files to [$outputPath]")
         compile(fileObjects)
         errorCount() == 0
     }
