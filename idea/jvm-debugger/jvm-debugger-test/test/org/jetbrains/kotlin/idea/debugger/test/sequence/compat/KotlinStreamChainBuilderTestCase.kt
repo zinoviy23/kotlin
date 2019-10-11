4f1d8a4a4ea5955c5b70bf8f6eedb181122af258
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2019-10-11 13:32:53 +0300

    -- another

--- idea/jvm-debugger/jvm-debugger-test/test/org/jetbrains/kotlin/idea/debugger/test/sequence/compat/KotlinStreamChainBuilderTestCase.kt
+++ idea/jvm-debugger/jvm-debugger-test/test/org/jetbrains/kotlin/idea/debugger/test/sequence/compat/KotlinStreamChainBuilderTestCase.kt
@@ -6,6 +6,12 @@
 package org.jetbrains.kotlin.idea.debugger.test.sequence.compat
 
 import com.intellij.debugger.streams.test.StreamChainBuilderTestCase
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
 
 // BUNCH: 192
-typealias KotlinStreamChainBuilderTestCase = StreamChainBuilderTestCase
+@Suppress("IncompatibleAPI")
+abstract class KotlinStreamChainBuilderTestCase : StreamChainBuilderTestCase() {
+    val project_: Project get() = project
+    val module_: Module get() = module
+}
