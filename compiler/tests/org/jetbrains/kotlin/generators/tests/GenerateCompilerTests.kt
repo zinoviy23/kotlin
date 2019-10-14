6c4e4859fc8c4c46a17df6658ab89c988cee1a9d
Leonid Startsev <leonid.startsev@jetbrains.com>
2019-10-14 20:20:48 +0300

    Tests for explicit api infrastructure setup

--- compiler/tests/org/jetbrains/kotlin/generators/tests/GenerateCompilerTests.kt
+++ compiler/tests/org/jetbrains/kotlin/generators/tests/GenerateCompilerTests.kt
@@ -109,6 +109,10 @@ fun main(args: Array<String>) {
             model("diagnostics/testsWithUnsignedTypes")
         }
 
+        testClass<AbstractDiagnosticsWithExplicitApi> {
+            model("diagnostics/testsWithExplicitApi")
+        }
+
         testClass<AbstractMultiPlatformIntegrationTest> {
             model("multiplatform", extension = null, recursive = true, excludeParentDirs = true)
         }
