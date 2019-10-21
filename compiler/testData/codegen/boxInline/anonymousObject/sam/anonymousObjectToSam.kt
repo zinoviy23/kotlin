b49906fd719403b17a6421a59d54af330b02c5c9
pyos <pyos@google.com>
2019-10-21 14:25:30 +0200

    JVM_IR: create temporaries for complex SAM conversion arguments
    To avoid bytecode sequences like
    
        new _1Kt$sam$i$java_lang_Runnable$0
        dup
        new _1Kt$f$1
        dup
        invokespecial _1Kt$f$1.<init>()V
        invokespecial _1Kt$sam$i$java_lang_Runnable$0.<init>(...)V
    
    as the different order of `new` and `<init>` confuses the inliner.

---
+++ compiler/testData/codegen/boxInline/anonymousObject/sam/anonymousObjectToSam.kt
@@ -0,0 +1,12 @@
+// IGNORE_BACKEND: JVM_IR
+// FILE: 1.kt
+inline fun f(crossinline g: () -> Unit) = Runnable(object : () -> Unit {
+    override fun invoke() = g()
+})
+
+// FILE: 2.kt
+fun box(): String {
+    var result = "FAIL"
+    f { result = "OK" }.run()
+    return result
+}
